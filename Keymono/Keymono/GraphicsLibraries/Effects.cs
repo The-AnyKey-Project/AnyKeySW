58c74d93cc87a5e114621ec477cbed31042073c5
Johannes Ringe <Ringe.Johannes@mh-hannover.de>
2021-01-22 22:56:10 +0100

    Keymono Testproject added

---
+++ Keymono/Keymono/GraphicsLibraries/Effects.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Keymono.GraphicsLibraries
+{
+    class Effects
+    {
+
+        static float[][] gray_matrix = new float[][] {
+                    new float[] { 0.299f, 0.299f, 0.299f, 0, 0 },
+                    new float[] { 0.587f, 0.587f, 0.587f, 0, 0 },
+                    new float[] { 0.114f, 0.114f, 0.114f, 0, 0 },
+                    new float[] { 0,      0,      0,      1, 0 },
+                    new float[] { 0,      0,      0,      0, 1 }
+                };
+
+        static float[][] bw_matrix = new float[][] {
+                    new float[] {1.5f, 1.5f, 1.5f, 0, 0},
+                    new float[] {1.5f, 1.5f, 1.5f, 0, 0},
+                    new float[] {1.5f, 1.5f, 1.5f, 0, 0},
+                    new float[] {0f, 0f, 0f, 1f, 0f},
+                    new float[] {-1f, -1f, -1f, 0f, 1f}
+                };
+
+        public static Image ConvertToGrayScale(Image SourceImage, float Threshold)
+        {
+            return ConvertColorMatrix(SourceImage, gray_matrix, Threshold);
+        }
+
+        public static Image ConvertToBlackAndWhite(Image SourceImage, float Threshold)
+        {
+                return ConvertColorMatrix(ReplaceTransparency(SourceImage, Color.Black), bw_matrix, Threshold);
+            
+        }
+
+        private static Image ConvertColorMatrix(Image SourceImage, float[][] ColorMatrix, float Threshold)
+        {
+            Bitmap work = new Bitmap(SourceImage);
+
+            using (System.Drawing.Graphics gr = System.Drawing.Graphics.FromImage(work))
+            {
+                var ia = new System.Drawing.Imaging.ImageAttributes();
+                ia.SetColorMatrix(new System.Drawing.Imaging.ColorMatrix(bw_matrix));
+                ia.SetThreshold(Threshold); // Change this threshold as needed
+                var rc = new Rectangle(0, 0, SourceImage.Width, SourceImage.Height);
+                gr.DrawImage(SourceImage, rc, 0, 0, SourceImage.Width, SourceImage.Height, GraphicsUnit.Pixel, ia);
+            }
+            return work;
+        }
+
+        public static System.Drawing.Bitmap ReplaceTransparency(System.Drawing.Image image, System.Drawing.Color background)
+        {
+            return ReplaceTransparency((System.Drawing.Bitmap)image, background);
+        }
+
+        public static System.Drawing.Bitmap ReplaceTransparency(System.Drawing.Bitmap bitmap, System.Drawing.Color background)
+        {
+            /* Important: you have to set the PixelFormat to remove the alpha channel.
+             * Otherwise you'll still have a transparent image - just without transparent areas */
+            var result = new System.Drawing.Bitmap(bitmap.Size.Width, bitmap.Size.Height, System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+            var g = System.Drawing.Graphics.FromImage(result);
+
+            g.Clear(background);
+            g.CompositingMode = System.Drawing.Drawing2D.CompositingMode.SourceOver;
+            g.DrawImage(bitmap, 0, 0);
+
+            return result;
+        }
+        public static Bitmap ResizeImage(Bitmap bmp, int newWidth, int newHeight)
+        {
+            Bitmap newImage = new Bitmap(newWidth, newHeight);
+            using (System.Drawing.Graphics gr = System.Drawing.Graphics.FromImage(newImage))
+            {
+                gr.SmoothingMode = SmoothingMode.HighQuality;
+                gr.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                gr.PixelOffsetMode = PixelOffsetMode.HighQuality;
+                gr.DrawImage(bmp, new Rectangle(0, 0, newWidth, newHeight));
+            }
+            return newImage;
+        }
+    }
+}
---
+++ Keymono/Keymono/NativeLibraries/WindowActivator.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace KeyboardHookTest
+{
+    class WindowActivator
+    {
+
+
+        private const int SW_MAXIMIZE = 3;
+        private const int SW_MINIMIZE = 6;
+        private const int SW_RESTORE = 9;
+
+        private struct WINDOWPLACEMENT
+        {
+            public int length;
+            public int flags;
+            public int showCmd;
+            public System.Drawing.Point ptMinPosition;
+            public System.Drawing.Point ptMaxPosition;
+            public System.Drawing.Rectangle rcNormalPosition;
+        }
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool GetWindowPlacement(IntPtr hWnd, ref WINDOWPLACEMENT lpwndpl);
+
+        [DllImport("user32.dll")]
+        private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern bool SetForegroundWindow(IntPtr hWnd);
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool IsWindow(IntPtr hWnd);
+
+        [DllImport("user32.dll")]
+        static extern IntPtr GetActiveWindow();
+
+        [DllImport("user32.dll")]
+        static extern IntPtr GetForegroundWindow();
+
+        private enum WindowState
+        {
+            Unknown = 0,
+            Normal = 1,
+            Minimized = 2,
+            Maximized = 3
+        }
+
+        public string ProcessName
+        {
+            get; set;
+        }
+
+        public string Filename
+        {
+            get; set;
+        }
+
+        public string Arguments
+        {
+            get; set;
+        }
+
+        public bool ActivateOldProcess
+        {
+            get; set;
+
+        }
+
+        private Process Process;
+        private bool isRunning = false;
+        private IntPtr lastActiveWindow = IntPtr.Zero;
+        
+        public WindowActivator()
+        {
+            this.ProcessName = null;
+            this.Filename = "";
+            this.Arguments = null;
+            this.Process = null;
+
+        }
+
+        public bool Activate()
+        {
+            bool result = false;
+
+            if (ActivateOldProcess && GetProcessesByName(this.ProcessName))
+            {
+                isRunning = true;
+                ToggleWindow(this.Process.MainWindowHandle);
+            }
+            else
+            {
+                if (!isRunning)       // Prozess muss noch gestartet werden.
+                {
+
+                    this.Process = Process.Start(this.Filename, this.Arguments);
+
+                    try
+                    {
+                        this.Process.WaitForInputIdle();
+
+                    }
+                    catch (Exception ex)
+                    {
+                        ;
+                    }
+                    isRunning = ProcessExists(this.Process.Id);
+                    if (!isRunning)
+                    {
+                        Activate();
+                    }
+                    else
+                    {
+                        this.ProcessName = this.Process.ProcessName;
+                    }
+                }
+                else
+                {
+                    if (ProcessExists(this.Process.Id))
+                    {
+                        if (IsWindow(this.Process.MainWindowHandle))
+                        {
+                            ToggleWindow(this.Process.MainWindowHandle);
+                        }
+                        else
+                        {
+                            throw new Exception("MainWindowHandle not found!");
+                        }
+                    }
+                    else
+                    {
+                        isRunning = false;
+                        Activate();
+                        ;
+                    }
+                }
+            }
+            return result;
+        }
+
+        private void ToggleWindow(IntPtr Handle)
+        {
+            IntPtr activeWindow = GetForegroundWindow();
+
+            Console.WriteLine("Active Window : " + activeWindow.ToString());
+            Console.WriteLine("Current Handle: " + Handle.ToString());
+            Console.WriteLine("Last Handle   : " + lastActiveWindow.ToString());
+
+            if (activeWindow != Handle)
+            {
+                if (GetWindowState(Handle) == WindowState.Minimized)
+                    ShowWindow(Handle, SW_RESTORE);
+                SetForegroundWindow(Handle);
+                lastActiveWindow = activeWindow;
+            }
+            else
+            {
+                SetForegroundWindow(lastActiveWindow);
+                lastActiveWindow = Handle;
+            }
+
+        }
+
+        private WindowState GetWindowState(IntPtr Handle)
+        {
+            WINDOWPLACEMENT placement = new WINDOWPLACEMENT();
+            GetWindowPlacement(Handle, ref placement);
+            return (WindowState)placement.showCmd;
+            /*
+            switch (placement.showCmd)
+            {
+                case 1:
+                    return WindowState.Normal;
+                case 2:
+                    return WindowState.Minimized;
+                case 3:
+                    return WindowState.Maximized;
+            }
+            return WindowState.Unknown;
+            */
+        }
+
+        private bool ProcessExists(int id)
+        {
+            return Process.GetProcesses().Any(x => x.Id == id);
+        }
+        private bool GetProcessesByName(string ProcessName)
+        {
+            foreach (Process p in Process.GetProcessesByName(ProcessName))
+            {
+                this.Process = p;
+                return true;
+            }
+            return false;
+        }
+
+    }
+}
