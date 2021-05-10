ea8259a54846cccd56afe5be8a3f65395f66bbc5
j-zero <er@j-zero.de>
2021-05-10 14:05:50 +0200

    todo...

--- gnome-shell-plugin/anykeycompanion@datenpirat/extension.js
+++ gnome-shell-plugin/anykeycompanion@datenpirat/extension.js
@@ -1,59 +1,39 @@
-const Meta=imports.gi.Meta;
-const Shell=imports.gi.Shell;
+const Meta = imports.gi.Meta;
+const Shell = imports.gi.Shell;
 
-let focus_window_notify_connection=null;
-let window_title_notify_connection=null;
-let window=null;
+let focus_window_notify_connection = null;
 let tracker = null;
 
-function switch_layer()
+function init()
 {
-	if(!window)
-		return;
-	let title = window.get_title();
-	let app = tracker.get_window_app(window);
-	log("gtk app id: " + window.get_gtk_application_id());
-	log(app.get_name());
-	log(app.get_app_info().get_filename());
+	tracker = Shell.WindowTracker.get_default();
 }
-
-function on_focus_window_notify()
-{
-	if(window && window_title_notify_connection)
-		window.disconnect(window_title_notify_connection);
-
-	window=global.display.get_focus_window();
-
-	if(!window)
-		return;
-
-	let app = tracker.get_window_app(window);
-
-	log("AnyKeyCompanion get_appname(): " + app.get_name());
-	log("AnyKeyCompanion get_app_info().get_filename(): " + app.get_app_info().get_filename());
-
-//	window_title_notify_connection=window.connect("notify::title",switch_layer);
-//	switch_layer();
-}
-
 
 function enable()
 {
-	focus_window_notify_connection=global.display.connect('notify::focus-window',on_focus_window_notify);
-	on_focus_window_notify();
+	focus_window_notify_connection=global.display.connect('notify::focus-window',on_notify_focus_window);
+	on_notify_focus_window();
 }
 
 function disable()
 {
 	global.display.disconnect(focus_window_notify_connection);
-
-	if(window && window_title_notify_connection)
-		window.disconnect(window_title_notify_connection);
-
-	AppMenu._sync();
 }
 
-function init()
+function on_notify_focus_window()
 {
-	tracker = Shell.WindowTracker.get_default();
+	let window=global.display.get_focus_window();
+
+	if(!window)
+		return;
+
+	let app = tracker.get_window_app(window);
+	let app_name = app.get_name();
+
+	log("AnyKeyCompanion App Name: '" + app_name + "'");
+	//log("AnyKeyCompanion get_app_info().get_filename(): " + app.get_app_info().get_filename()); // only GTK apps
+	//log("AnyKeyCompanion get_app_info().get_generic_name(): " + app.get_app_info().get_generic_name()); // only GTK apps
+
+	// TODO: do something with app_name
+	
 }
